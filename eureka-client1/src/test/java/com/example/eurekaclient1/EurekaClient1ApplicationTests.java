7ea20d0ac73d90835453f38c47cdeec80675f9e2
xujiangdong <xujiangdong@changingedu.com>
2019-11-22 17:32:20 +0800

    1

---
+++ eureka-client1/src/test/java/com/example/eurekaclient1/EurekaClient1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.eurekaclient1;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
