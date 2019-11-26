25b42634568b491ec5d2d7a695dee8c0a7c28a73
xujiangdong <xujiangdong@changingedu.com>
2019-11-26 11:46:51 +0800

    add project:service-feign

---
+++ service-feign/src/main/java/com/example/servicefeign/FeignService.java
@@ -0,0 +1,18 @@
+package com.example.servicefeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author XuJD
+ * @create 2019-11-26 11:13
+ **/
+@FeignClient(value = "eureka-client1")
+@Service
+public interface FeignService {
+
+    @RequestMapping(value = "/serviceTest/test")
+    String test();
+
+}
