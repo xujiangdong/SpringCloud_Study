16ef3b6ac855ce20b7ae89e943524c7c89723109
xujiangdong <xujiangdong@changingedu.com>
2019-11-28 11:45:50 +0800

    引入断路器Hystrix

--- service-ribbon/src/main/java/com/example/serviceribbon/TestService.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/TestService.java
@@ -1,5 +1,6 @@
 package com.example.serviceribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,12 @@ public class TestService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "error")
     public String testService(){
         return restTemplate.getForObject("http://eureka-client1/serviceTest/test",String.class);
+    }
+
+    public String error(){
+        return " error";
     }
 }
