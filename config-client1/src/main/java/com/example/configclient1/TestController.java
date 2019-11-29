93c1e41a963da3c8bc6dc3178b1c4bdd26551fc9
xujiangdong <xujiangdong@changingedu.com>
2019-11-29 11:50:11 +0800

    配置中心客户端

---
+++ config-client1/src/main/java/com/example/configclient1/TestController.java
@@ -0,0 +1,21 @@
+package com.example.configclient1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author XuJD
+ * @create 2019-11-28 17:21
+ **/
+@RestController
+public class TestController {
+
+    @Value("${db.url}")
+    private String url;
+
+    @RequestMapping("getConfig")
+    public String getConfig(){
+        return url;
+    }
+}
---
+++ config-client1/src/test/java/com/example/configclient1/ConfigClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configclient1;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
