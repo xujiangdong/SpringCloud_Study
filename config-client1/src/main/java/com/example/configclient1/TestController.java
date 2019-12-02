c72fb2f17e5c750dfd453413fcf256e7e27c5920
xujiangdong <xujiangdong@changingedu.com>
2019-12-02 09:28:49 +0800

    引入消息总线

--- config-client1/src/main/java/com/example/configclient1/TestController.java
+++ config-client1/src/main/java/com/example/configclient1/TestController.java
@@ -1,6 +1,7 @@
 package com.example.configclient1;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2019-11-28 17:21
  **/
 @RestController
+@RefreshScope
 public class TestController {
 
     @Value("${db.url}")
